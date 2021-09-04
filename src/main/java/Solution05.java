8c387cc1f5cdfd6663e35a713845c47921fc6509
Alexander <azoomer2@gmail.com>
2021-09-04 11:07:12 -0400

    Initial Push

---
+++ src/main/java/Solution05.java
@@ -0,0 +1,19 @@
+public class Solution05 {
+    public static void main(String[] args) {
+
+    }
+}
+
+/* Pseudo Code
+
+    System.out prompt user for 'num1'
+    System.in 'num1'
+    System.out prompt user for 'num2'
+    System.in 'num2'
+    sum = num1 + num2
+    diff = num1 - num2
+    product = num1 * num2
+    quotient = num1 / num2
+    System.out results with formatting
+
+ */
